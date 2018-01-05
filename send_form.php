bf09e73cf7301468a3f64adc95b1a59a892ea8c3
nyanloutre <paul.trehiou@protonmail.com>
2018-01-05 17:56:30 +0100

    Ajout formulaire de contact

---
+++ send_form.php
@@ -0,0 +1,30 @@
+<?php
+//Redirect to home page after 5 seconds
+header('Refresh: 5; URL=/');
+if(isset($_POST['submit']) && !empty($_POST['submit'])):
+    //contact form submission code
+    $name = !empty($_POST['name'])?$_POST['name']:'';
+    $email = !empty($_POST['email'])?$_POST['email']:'';
+    $message = !empty($_POST['message'])?$_POST['message']:'';
+    
+    $to = 'contact@enviro-consult.fr';
+    $subject = 'Un nouveau formulaire à été soumis';
+    $htmlContent = "
+        <h1>Details du formulaire</h1>
+        <p><b>Nom: </b>".$name."</p>
+        <p><b>E-Mail: </b>".$email."</p>
+        <p><b>Message: </b>".$message."</p>
+    ";
+    // Always set content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    // More headers
+    $headers .= 'From:'.$name.' <'.$email.'>' . "\r\n";
+    //send email and shut errors
+    @mail($to,$subject,$htmlContent,$headers);
+    
+    echo '<h2>Envoi réussi vous allez être redirigé vers la page d\'accueil dans 5 secondes</h2>';
+else
+    echo '<h2>Echec de la requète vous allez être redirigé vers la page d\'accueil dans 5 secondes</h2>';
+endif;
+?>
